fcc808ff22f53c29f14b3c9ab10d093c514d08d1
JieQiangG <jieqiang@ucm.es>
2025-03-10 23:02:52 +0100

    mov 8 dir y empezando refactoring

--- src/player_warrior.js
+++ src/player_warrior.js
@@ -35,29 +35,14 @@ export default class Player_warrior extends Phaser.GameObjects.Sprite {
         this.hp=10;
         this.maxHp=10;
 
+        this.addAttackArea("circle");
 
-        /*this.attackArea=this.scene.add.graphics();
-        this.attackArea.lineStyle(2, 0xFFFFFF, 1);
-        this.attackArea.fillStyle(0xff0000,0.2);
-        this.attackArea.fillRect(this.x-64,this.y-64,128,128);
-        this.attackArea.strokeRect(this.x-64,this.y-64,128,128);*/
-        //this.attackArea=this.scene.add.rectangle(this.x,this.y+5, 128, 128, 0xff0000,0.25).setVisible(false);
-        this.attackArea=this.scene.add.circle(this.x,this.y+5, 64, 0xff0000,0.25).setVisible(false);
-        this.attackArea.setStrokeStyle(1, 0xff0000, 1);
-        this.attackArea.setDepth(this.depth+1);
-        this.moveArea=new Phaser.Geom.Circle(this.x,this.y+14,100)//this.scene.add.rectangle(this.x,this.y+5, 200, 200, 0x0080ff,0.25);
-
+        this.moveArea=new Phaser.Geom.Circle(this.x,this.y+14,100)
         this.moveAreaGraphics=this.scene.add.graphics().setVisible(false);
         this.moveAreaGraphics.lineStyle(1, 0x0069ff, 0.50);  
         this.moveAreaGraphics.fillStyle("0x0069ff",0.20);
         this.moveAreaGraphics.fillCircleShape(this.moveArea);
         this.moveAreaGraphics.strokeCircleShape(this.moveArea);
-
-        console.log(this.moveArea);
-        console.log(this);
-
-
-
 
         console.log()
         //this.scene.physics.add(this.attackArea);
@@ -215,41 +200,37 @@ export default class Player_warrior extends Phaser.GameObjects.Sprite {
         this.body.setVelocityX(0);
         this.body.setVelocityY(0);
 
-        if (this.cursors.up.isDown) {
+        if (this.cursors.up.isDown && !this.cursors.down.isDown) {
             this.facing="up";
             if(this.moveArea.contains(this.body.center.x,this.body.top)){
-                this.body.setVelocityY(-velocity);
-                this.body.setVelocityX(0);
-                this.play({key:'walk_up'},true);
+                this.body.setVelocityY(-1);
+                if(!this.cursors.right.isDown && !this.cursors.left.isDown)this.play({key:'walk_up'},true);
             }else{
                 this.scene.playCollideEffect();
             }
         }
-        else if (this.cursors.down.isDown) {
+        else if (this.cursors.down.isDown && !this.cursors.up.isDown) {
             this.facing="down";
             if(this.moveArea.contains(this.body.center.x,this.body.bottom)){
-                this.body.setVelocityY(velocity);
-                this.body.setVelocityX(0);
-                this.play({key:'walk_down'},true);
+                this.body.setVelocityY(1);
+                if(!this.cursors.right.isDown && !this.cursors.left.isDown)this.play({key:'walk_down'},true);
             }else{
                 this.scene.playCollideEffect();
             }
         } 
-        else if (this.cursors.right.isDown) {
+        if (this.cursors.right.isDown && !this.cursors.left.isDown) {
             this.facing="right";
             if(this.moveArea.contains(this.body.right,this.body.center.y)){
-                this.body.setVelocityX(velocity);
-                this.body.setVelocityY(0);
+                this.body.setVelocityX(1);
                 this.play({key:'walk_right'},true);
             }else{
                 this.scene.playCollideEffect();
             }
         }
-        else if (this.cursors.left.isDown) {
+        if (this.cursors.left.isDown && !this.cursors.right.isDown) {
             this.facing="left";
             if(this.moveArea.contains(this.body.left,this.body.center.y)){
-                this.body.setVelocityX(-velocity);
-                this.body.setVelocityY(0);
+                this.body.setVelocityX(-1);
                 this.play({key:'walk_left'},true);
             }else{
                 this.scene.playCollideEffect();
@@ -257,6 +238,8 @@ export default class Player_warrior extends Phaser.GameObjects.Sprite {
         }
         if(this.body.velocity.x==0 && this.body.velocity.y==0 && this.isMoving==false){
             this.play({key:'iddle_'+this.facing,repeat:-1},true);
+        }else{
+            this.body.velocity.setLength(velocity);
         }
 
     }
@@ -416,6 +399,30 @@ export default class Player_warrior extends Phaser.GameObjects.Sprite {
         this.setDepth(this.depth-1);
     }
 
+
+    addAttackArea(type){
+        switch(type){
+            case "circle":
+                this.attackAreaOffsetX=0;
+                this.attackAreaOffsetY=14;
+                this.attackArea=this.scene.add.circle(this.x+this.attackAreaOffsetX,this.y+this.attackAreaOffsetY, 64, 0xff0000,0.25).setVisible(false);
+                this.attackArea.setStrokeStyle(1, 0xff0000, 1);
+                this.attackArea.setDepth(this.depth+1);
+                this.scene.physics.add.existing(this.attackArea);
+                this.attackArea.body.setCircle(64);
+            break;
+            case "rectangle":
+                this.attackAreaOffsetX=0;
+                this.attackAreaOffsetY=14;
+                this.attackArea=this.scene.add.rectangle(this.x+ this.attackAreaOffsetX,this.y+this.attackAreaOffsetY, 128,128, 0xff0000,0.25).setVisible(false);
+                this.attackArea.setStrokeStyle(1, 0xff0000, 1);
+                this.attackArea.setDepth(this.depth+1);
+                this.scene.physics.add.existing(this.attackArea);
+            break;
+            
+        }
+    }
+
     /**
      * Gestiona movimiento de teletransporte
      */
@@ -443,8 +450,8 @@ export default class Player_warrior extends Phaser.GameObjects.Sprite {
         if(this.scene.activeCharacter=="warrior")this.physics_combat_movement(t);
         this.container.x=this.scene.pointerGridX;
         this.container.y=this.scene.pointerGridY;
-        this.attackArea.x= this.x;
-        this.attackArea.y=this.y+14;
+        this.attackArea.x= this.x+this.attackAreaOffsetX;
+        this.attackArea.y=this.y+this.attackAreaOffsetY;
     }
 
 }
