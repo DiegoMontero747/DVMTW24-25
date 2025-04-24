2d250e0967ff736723a629cf4e3704faa1ea7688
DiegoMontero747 <diemon01@ucm.es>
2025-04-24 12:53:00 +0200

    preparaciones portal de salida y pasar experiencia

--- src/level3.js
+++ src/level3.js
@@ -37,6 +37,15 @@ export default class Level3 extends Phaser.Scene {
     constructor() {
         super({ key: 'level3' });
     }
+
+    constructor(hp, level, exp) {
+        this.playerData = {
+            hp: hp,
+            level: level,
+            exp: exp
+        }
+        super({ key: 'level3' });
+    }
     /**
      * Creación de los elementos de la escena principal de juego
      */
